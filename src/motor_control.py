1a6e8ac425d4b32b90c8bad40fc87daf5766055f
Zachery Uporsky <zuporsky@gmail.com>
2021-04-28 09:44:14 -0400

    Added few function stubs and began calibrate function.

--- src/motor_control.py
+++ src/motor_control.py
@@ -5,11 +5,20 @@ from time import sleep
 #Author: Zachery Uporsky
 #Date: 4/28/21
 
-pinConfig  #pin config
+#CLASS VARIABLES
+
+pinConfig = [] #pin config
+pinConfigA = [] #left L298N
+pinConfigB = [] #right L298N
+
 
 
 #Inits pin configuration , needs all pins configured per driver 
+#Groups of 2 IN's and 1 EN pin
+#Start all outputs as low
+
 def initPinConfig (IN1, IN2, IN3, IN4, ENA, ENB):
+    
     GPIO.setMode(GPIO.BCM)
 
     #first side of L298N
@@ -30,18 +39,28 @@ def initPinConfig (IN1, IN2, IN3, IN4, ENA, ENB):
     p2=GPIO.PWM(ENB,1000)
     p2.start(25)
 
-
+    print("Pin configurations initialized. Pins have been initialized as the following: ")
+    print("IN1: " + IN1 +"\nIN2: " + IN2 + "\nENA: " + ENA + "\nIN3: " + IN3 + "\nIN4: " + IN4 + "\nENB: " + ENB)
     return [IN1, IN2, IN3, IN4, ENA, ENB]
     
-
+    #function stubs
+def calibrateMotors ():
+    if not(pingConfigA == [] and pingConfigB == []):
+        GPIO.output(pinConfigA[0], GPIO.HIGH)
+        #then record rps with sensor
+        
+    pass
 
 
 def forward():
     pass
 
+
+
 def reverse():
     pass
 
+#overload deg type, "RIGHT", "LEFT"
 def forward(type):
     return True
     
@@ -56,4 +75,5 @@ def stop():
 
 if __name__ == "__main__":
     GPIO.cleanup()
+    
     pass
